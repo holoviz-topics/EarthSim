1127ddc1f7ae3199d928fcdd6eedbd5458bc64ea
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-03-12 14:58:28 +0000

    Fixed initialization of TriMesh cross-sectors

--- earthsim/analysis.py
+++ earthsim/analysis.py
@@ -73,7 +73,10 @@ class LineCrossSection(param.Parameterized):
         a variable number of elements batching must be enabled and
         the legend_limit must be set to 0.
         """
-        path = self.path_stream.element
+        if self.path_stream.data is None:
+            path = self.path
+        else:
+            path = self.path_stream.element
         if isinstance(obj, TriMesh): 
             vdim = obj.nodes.vdims[0]
         else:
@@ -152,7 +155,10 @@ class SurfaceCrossSection(LineCrossSection):
         Rasterizes the supplied object across times returning
         an Image of the sampled data across time and distance.
         """
-        path = self.path_stream.element
+        if self.path_stream.data is None:
+            path = self.path
+        else:
+            path = self.path_stream.element
         if isinstance(obj, TriMesh): 
             vdim = obj.nodes.vdims[0]
         else:
