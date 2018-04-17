389d7617eef6588a81d09eeb93805af95dd9c8fd
C. E. Ball <ceball@gmail.com>
2018-04-17 12:25:51 +0100

    Initial pass at using pyct.

--- dodo.py
+++ dodo.py
@@ -1,33 +1,14 @@
-DOIT_CONFIG = {'verbosity': 2}
+import os
+if "PYCT_ECOSYSTEM" not in os.environ:
+    os.environ["PYCT_ECOSYSTEM"] = "conda"
 
-import shutil
-from doit import action
-from ioamdoit import *
-
-# The aim would be to not have anything much here, but right now
-# that's usually not possible because of awkward/non-standard ways of
-# installing dependencies, running tests, downloading data, etc,
-# across projects.
-
-def task_test_nb():
-    return {'actions': ['pytest --nbsmoke-run examples/']}
-
-def task_all_tests():
-    return {'actions': [],
-            'task_dep': ['test_nb']}
+from pyct import *  # noqa: api
 
 
 ############################################################
-# Website building tasks
-#
-# More complex than necessary because nbsite doesn't yet have a conda
-# package (assumes notebook, ipython, etc already installed), and because
-# nbsite itself does not use doit internally (or provide a dodo file for docs).
+# Website building tasks; will move out to pyct
 
-def task_install_doc_dependencies():
-    return {
-        'actions': [
-            'conda install -y -c pyviz/label/dev -c conda-forge nbsite sphinx_ioam_theme']}
+from doit import action
 
 def task_download_sample_data():
     return {
