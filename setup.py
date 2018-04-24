5c88eb66841bfd4b9f5c83fba2ffa768b65de2c3
Chris B <ceball@gmail.com>
2018-04-24 11:28:26 +0100

    Updated to autover's new setup.py functions.

--- setup.py
+++ setup.py
@@ -3,46 +3,52 @@ from setuptools import setup, find_packages
 
 ########## autover ##########
 
-def embed_version(basepath, ref='v0.2.2'):
+def embed_version(basepath, ref='v0.2.4'):
     """
     Autover is purely a build time dependency in all cases (conda and
     pip) except for when you use pip's remote git support [git+url] as
     1) you need a dynamically changing version and 2) the environment
     starts off clean with zero dependencies installed.
+
     This function acts as a fallback to make Version available until
     PEP518 is commonly supported by pip to express build dependencies.
     """
-    import io, zipfile, importlib
+    import io, zipfile, os
     try:    from urllib.request import urlopen
     except: from urllib import urlopen
-    try:
-        url = 'https://github.com/ioam/autover/archive/{ref}.zip'
-        response = urlopen(url.format(ref=ref))
-        zf = zipfile.ZipFile(io.BytesIO(response.read()))
-        ref = ref[1:] if ref.startswith('v') else ref
-        embed_version = zf.read('autover-{ref}/autover/version.py'.format(ref=ref))
-        with open(os.path.join(basepath, 'version.py'), 'wb') as f:
-            f.write(embed_version)
-        return importlib.import_module("version")
-    except:
-        return None
+    response = urlopen('https://github.com/ioam/autover/archive/{ref}.zip'.format(ref=ref))
+    zf = zipfile.ZipFile(io.BytesIO(response.read()))
+    ref = ref[1:] if ref.startswith('v') else ref
+    embed_version = zf.read('autover-{ref}/autover/version.py'.format(ref=ref))
+    with open(os.path.join(basepath, 'version.py'), 'wb') as f:
+        f.write(embed_version)
 
-def get_setup_version(reponame):
+
+def get_setup_version(reponame, pkgname=None):
     """
     Helper to get the current version from either git describe or the
-    .version file (if available).
+    .version file (if available). Set pkgname to the package name if it
+    is different from the repository name.
     """
-    import json
-    basepath = os.path.split(__file__)[0]
-    version_file_path = os.path.join(basepath, reponame, '.version')
-    try:
-        from param import version
+    import json, importlib, os
+    pkgname = reponame if pkgname is None else pkgname
+    basepath = os.path.dirname(os.path.abspath(__file__))
+    version_file_path = os.path.join(basepath, pkgname, '.version')
+    version = None
+    try: version = importlib.import_module("version") # bundled
     except:
-        version = embed_version(basepath)
+        try: from autover import version # available as package
+        except:
+            try: from param import version # available via param
+            except:
+                embed_version(basepath) # download
+                version = importlib.import_module("version")
+
     if version is not None:
-        return version.Version.setup_version(basepath, reponame, archive_commit="$Format:%h$")
+        return version.Version.setup_version(basepath, reponame, pkgname=pkgname,
+                                             archive_commit="$Format:%h$")
     else:
-        print("WARNING: param>=1.6.0 unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should install param>=1.6.0.")
+        print("WARNING: autover unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should refer to autover's documentation to bundle autover or add it as a dependency.")
         return json.load(open(version_file_path, 'r'))['version_string']
 
 
@@ -99,11 +105,10 @@ extras_require = {
 
 ########## metadata for setuptools ##########
 
-setup_args = {}
+setup_args = {'name':'earthsim'}
 
 setup_args.update(dict(
-    name='earthsim',
-    version=get_setup_version("EarthSim"),
+    version=get_setup_version("EarthSim",pkgname=setup_args['name']),
     packages = find_packages(),
     include_package_data=True,
     install_requires = install_requires,
