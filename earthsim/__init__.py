9212df2435d0b917d54b88e57ad9c26f6da78e23
Chris B <ceball@gmail.com>
2018-04-27 22:24:31 +0100

    Make missing command message work.

--- earthsim/__init__.py
+++ earthsim/__init__.py
@@ -8,7 +8,7 @@ try:
     install_examples = partial(_examples,'earthsim')
     download_data = partial(_data,'earthsim')
 except ImportError:
-    def _missing_cmd(*args,**kw): print("install pvutil to enable this command")
+    def _missing_cmd(*args,**kw): return("install pvutil to enable this command")
     _data = _examples = _missing_cmd
     def _err(): raise ValueError(_missing_cmd())
     download_data = install_examples = _err
