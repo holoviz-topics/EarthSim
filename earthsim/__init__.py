32eaeb4cc75e8f531f20b411237519ca0a76d7ee
Chris B <ceball@gmail.com>
2018-05-21 21:39:53 +0100

    pvutil has been renamed to pyct

--- earthsim/__init__.py
+++ earthsim/__init__.py
@@ -1,14 +1,14 @@
 import param
 __version__ = str(param.version.Version(fpath=__file__, archive_commit="$Format:%h$",reponame="EarthSim"))
 
-# make pvutil's install_examples() and download_data() available if possible
+# make pyct's install_examples() and download_data() available if possible
 from functools import partial
 try:
-    from pvutil.cmd import install_examples as _examples, download_data as _data
+    from pyct.cmd import install_examples as _examples, download_data as _data
     install_examples = partial(_examples,'earthsim')
     download_data = partial(_data,'earthsim')
 except ImportError:
-    def _missing_cmd(*args,**kw): return("install pvutil to enable this command (e.g. `conda install -c pyviz pvutil`)")
+    def _missing_cmd(*args,**kw): return("install pvct to enable this command (e.g. `conda install -c pyviz pyct`)")
     _data = _examples = _missing_cmd
     def _err(): raise ValueError(_missing_cmd())
     download_data = install_examples = _err
