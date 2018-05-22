18ed96a22348ed2c0ddc55832e4c135c06061fc9
Chris B <ceball@gmail.com>
2018-05-22 22:12:41 +0100

    Update examples commands to use pyct after package rename (changes missed in previous commits).

--- earthsim/__init__.py
+++ earthsim/__init__.py
@@ -1,15 +1,16 @@
 import param
 __version__ = str(param.version.Version(fpath=__file__, archive_commit="$Format:%h$",reponame="EarthSim"))
 
-# make pyct's install_examples() and download_data() available if possible
+# make pyct's example/data commands available if possible
 from functools import partial
 try:
-    from pyct.cmd import install_examples as _examples, download_data as _data
-    install_examples = partial(_examples,'earthsim')
-    download_data = partial(_data,'earthsim')
+    from pyct.cmd import copy_examples as _copy, fetch_data as _fetch, examples as _examples
+    copy_examples = partial(_copy,'earthsim')
+    fetch_data = partial(_fetch,'earthsim')
+    examples = partial(_examples, 'earthsim')
 except ImportError:
     def _missing_cmd(*args,**kw): return("install pyct to enable this command (e.g. `conda install -c pyviz pyct`)")
-    _data = _examples = _missing_cmd
+    _copy = _fetch = _examples = _missing_cmd
     def _err(): raise ValueError(_missing_cmd())
-    download_data = install_examples = _err
-del partial, _examples, _data
+    fetch_data = copy_examples = examples = _err
+del partial, _examples, _copy, _fetch
--- earthsim/__main__.py
+++ earthsim/__main__.py
@@ -2,12 +2,12 @@ import sys
 
 def main(args=None):
     try:
-        import pvutil.cmd
+        import pyct.cmd
     except ImportError:
         from . import _missing_cmd
         print(_missing_cmd())
         sys.exit(1)
-    return pvutil.cmd.substitute_main('earthsim',args=args)
+    return pyct.cmd.substitute_main('earthsim',args=args)
 
 if __name__ == "__main__":
     main()
