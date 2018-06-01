095c2595692db07d33cf0056369e09b9e4b45bc6
jlstevens <jlstevens@ed.ac.uk>
2018-06-01 02:02:31 +0100

    Registered entry point for the console script

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup_args.update(dict(
     name='earthsim',
     version="0.1",
     packages = find_packages(),
+    entry_points={
+          'console_scripts': [
+              'param = earthsim.__main__:main'
+          ]}
 ))
 
 if __name__=="__main__":
