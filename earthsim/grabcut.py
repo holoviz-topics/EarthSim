dacaa75766705d9b6f398e8d5865f1d408f5428a
Philipp Rudiger <philippjfr@continuum.io>
2018-06-25 11:40:01 +0100

    Improved GrabCut dashboard (#125)

--- earthsim/grabcut.py
+++ earthsim/grabcut.py
@@ -5,6 +5,7 @@ import numpy as np
 import holoviews as hv
 import geoviews as gv
 import cartopy.crs as ccrs
+import datashader as ds
 
 from PIL import Image, ImageDraw
 from holoviews.core.operation import Operation
@@ -47,6 +48,9 @@ class extract_foreground(Operation):
 
     background = param.ClassSelector(class_=hv.Path)
 
+    iterations = param.Integer(default=5, bounds=(0, 20), doc="""
+        Number of iterations to run the GrabCut algorithm for.""")
+
     def _process(self, element, key=None):
         try:
             import cv2 as cv
@@ -56,14 +60,14 @@ class extract_foreground(Operation):
         if isinstance(self.p.foreground, hv.Polygons):
             rasterize_op = rasterize_polygon
         else:
-            rasterize_op = rasterize.instance(aggregator='any')
+            rasterize_op = rasterize.instance(aggregator=ds.any())
 
         kwargs = {'dynamic': False, 'target': element}
         fg_mask = rasterize_op(self.p.foreground, **kwargs)
         bg_mask = rasterize_op(self.p.background, **kwargs)
         fg_mask = fg_mask.dimension_values(2, flat=False)
         bg_mask = bg_mask.dimension_values(2, flat=False)
-        if fg_mask.sum() == 0 or bg_mask.sum() == 0:
+        if fg_mask[np.isfinite(fg_mask)].sum() == 0 or bg_mask[np.isfinite(bg_mask)].sum() == 0:
             return element.clone([], vdims=['Foreground'], new_type=gv.Image,
                                  crs=element.crs)
 
@@ -71,18 +75,35 @@ class extract_foreground(Operation):
         mask = np.where(bg_mask, 0, mask).copy()
         bgdModel = np.zeros((1,65), np.float64)
         fgdModel = np.zeros((1,65), np.float64)
-        
+
         if isinstance(element, hv.RGB):
             img = np.dstack([element.dimension_values(d, flat=False)
                              for d in element.vdims])
         else:
             img = element.dimension_values(2, flat=False)
-        mask, _, _ = cv.grabCut(img, mask.astype('uint8'), None, bgdModel, fgdModel, 5,
-                                cv.GC_INIT_WITH_MASK)
+        mask, _, _ = cv.grabCut(img, mask.astype('uint8'), None, bgdModel, fgdModel,
+                                self.p.iterations, cv.GC_INIT_WITH_MASK)
         fg_mask = np.where((mask==2)|(mask==0),0,1).astype('bool')
         xs, ys = (element.dimension_values(d, expanded=False) for d in element.kdims)
         return element.clone((xs, ys, fg_mask), vdims=['Foreground'], new_type=gv.Image,
                              crs=element.crs)
+
+
+
+class filter_polygons(Operation):
+
+    minimum_size = param.Integer(default=10)
+
+    def _process(self, element, key=None):
+        paths = []
+        for path in element.split():
+            if len(path) < self.p.minimum_size:
+                continue
+            for p in split_path(path):
+                if len(p) > self.p.minimum_size:
+                    paths.append(p)
+        return element.clone(paths)
+
 
 
 class GrabCutDashboard(Stream):
@@ -94,12 +115,15 @@ class GrabCutDashboard(Stream):
                               precedence=-1, doc="""
         Projection the inputs and output paths are defined in.""")
 
-    path_type = param.ClassSelector(default=gv.Polygons, class_=hv.Path,
+    path_type = param.ClassSelector(default=gv.Path, class_=hv.Path,
                                     precedence=-1, is_instance=False, doc="""
         The element type to draw into.""")
 
     update_contour = param.Action(default=lambda self: self.event(), precedence=1,
                                doc="""Button triggering GrabCut.""")
+
+    filter_contour = param.Action(default=lambda self: self.filter_stream.event(filter=True), precedence=1,
+                                  doc="""Button triggering GrabCut.""")
 
     width = param.Integer(default=500, precedence=-1, doc="""
         Width of the plot""")
@@ -110,6 +134,11 @@ class GrabCutDashboard(Stream):
     downsample = param.Magnitude(default=1., doc="""
         Amount to downsample image by before applying grabcut.""")
 
+    iterations = param.Integer(default=5, bounds=(0, 20), doc="""
+        Number of iterations to run the GrabCut algorithm for.""")
+
+    minimum_size = param.Integer(default=10)
+
     def __init__(self, image, fg_data=[], bg_data=[], **params):
         self.image = image
         super(GrabCutDashboard, self).__init__(transient=True, **params)
@@ -117,17 +146,22 @@ class GrabCutDashboard(Stream):
         self.fg_paths = gv.project(self.path_type(fg_data, crs=self.crs), projection=image.crs)
         self.draw_bg = PolyDraw(source=self.bg_paths)
         self.draw_fg = PolyDraw(source=self.fg_paths)
+        self.filter_stream = hv.streams.Stream.define('Filter', filter=False)(transient=True)
         self._initialized = False
+        self._filter = False
 
     def extract_foreground(self, **kwargs):
+        img = self.image
         if self._initialized:
             bg, fg = self.draw_bg.element, self.draw_fg.element
         else:
             self._initialized = True
             bg, fg = self.bg_paths, self.fg_paths
-        bg, fg = (gv.project(g, projection=self.image.crs) for g in (bg, fg))
+        bg, fg = (gv.project(g, projection=img.crs) for g in (bg, fg))
 
-        img = self.image
+        if not len(bg) or not len(fg):
+            return gv.Path([], img.kdims, crs=img.crs)
+
         if self.downsample != 1:
             kwargs = {'dynamic': False}
             h, w = img.interface.shape(img, gridded=True)
@@ -135,17 +169,24 @@ class GrabCutDashboard(Stream):
             kwargs['height'] = int(h*self.downsample)
             img = regrid(img, **kwargs)
 
-        foreground = extract_foreground(img, background=bg, foreground=fg)
+        foreground = extract_foreground(img, background=bg, foreground=fg, iterations=self.iterations)
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore')
-            foreground = gv.Path([contours(foreground, levels=1).split()[0].data],
+            foreground = gv.Path([contours(foreground, filled=True, levels=1).split()[0].data],
                                  kdims=foreground.kdims, crs=foreground.crs)
         self.result = gv.project(foreground, projection=self.crs)
         return foreground
+
+    def filter_contours(self, obj, **kwargs):
+        if kwargs.get('filter'):
+            obj = filter_polygons(obj, minimum_size=self.minimum_size)
+        self.result = obj
+        return obj
 
     def view(self):
         options = dict(width=self.width, height=self.height, xaxis=None, yaxis=None,
                        projection=self.image.crs)
         dmap = hv.DynamicMap(self.extract_foreground, streams=[self])
+        dmap = hv.util.Dynamic(dmap, operation=self.filter_contours, streams=[self.filter_stream])
         return (regrid(self.image).options(**options) * self.bg_paths * self.fg_paths +
                 dmap.options(**options)).options(merge_tools=False, clone=False)
