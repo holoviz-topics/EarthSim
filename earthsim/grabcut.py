0ae290ec19b76398b08b7349c5029e920774b60f
jlstevens <jlstevens@ed.ac.uk>
2018-09-06 23:00:59 +0100

    Formatting tweaks

--- earthsim/grabcut.py
+++ earthsim/grabcut.py
@@ -231,26 +231,28 @@ class SelectRegionPlot(param.Parameterized):
     Visualization that allows selecting a bounding box anywhere on a tile source.
     """
 
+    # Tile servers
     misc_servers =  {'OpenStreetMap' : 'http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png',
                      'Basemaps CartoCDN' :
                      'https://s.basemaps.cartocdn.com/light_all/{Z}/{X}/{Y}.png',
                      'Stamen':'http://tile.stamen.com/terrain/{Z}/{X}/{Y}.png'}
 
     arcgis_paths = {'World Imagery':'World_Imagery/MapServer/tile/{Z}/{Y}/{X}',
-                'World Topo Map':'World_Topo_Map/MapServer/tile/{Z}/{Y}/{X}',
-                'World Terrain Base':'World_Terrain_Base/MapServer/tile/{Z}/{Y}/{X}',
-                'World Street Map': 'World_Street_Map/MapServer/tile/{Z}/{Y}/{X}',
-                'World Shaded Relief': 'World_Shaded_Relief/MapServer/tile/{Z}/{Y}/{X}',
-                'World Physical Map': 'World_Physical_Map/MapServer/tile/{Z}/{Y}/{X}',
-                'USA Topo Maps' : 'USA_Topo_Maps/MapServer/tile/{Z}/{Y}/{X}',
-                'Ocean Basemap' : 'Ocean_Basemap/MapServer/tile/{Z}/{Y}/{X}',
-                'NatGeo World Map' : 'NatGeo_World_Map/MapServer/tile/{Z}/{Y}/{X}'}
+                    'World Topo Map':'World_Topo_Map/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Terrain Base':'World_Terrain_Base/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Street Map': 'World_Street_Map/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Shaded Relief': 'World_Shaded_Relief/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Physical Map': 'World_Physical_Map/MapServer/tile/{Z}/{Y}/{X}',
+                    'USA Topo Maps' : 'USA_Topo_Maps/MapServer/tile/{Z}/{Y}/{X}',
+                    'Ocean Basemap' : 'Ocean_Basemap/MapServer/tile/{Z}/{Y}/{X}',
+                    'NatGeo World Map' : 'NatGeo_World_Map/MapServer/tile/{Z}/{Y}/{X}'}
 
     arcgis_urls = {k: 'https://server.arcgisonline.com/ArcGIS/rest/services/' + v
                    for k,v in arcgis_paths.items()}
     tile_urls = dict(misc_servers, **arcgis_urls)
 
 
+    # Parameters
     name = param.String(default='Region Settings')
 
     width = param.Integer(default=900, precedence=-1, doc="Width of the plot in pixels")
