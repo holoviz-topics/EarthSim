1dc085a81d685a3650da83844b8bab389b21e299
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-10-04 14:14:26 +0100

    Started conversion from parambokeh to panel

--- earthsim/grabcut.py
+++ earthsim/grabcut.py
@@ -1,3 +1,5 @@
+import os
+import math
 import warnings
 
 import param
@@ -6,14 +8,16 @@ import holoviews as hv
 import geoviews as gv
 import cartopy.crs as ccrs
 import datashader as ds
+import quest
 
 from PIL import Image, ImageDraw
 from holoviews.core.operation import Operation
+from holoviews.core.options import Store, Options
 from holoviews.core.util import pd
 from holoviews.element.util import split_path
 from holoviews.operation.datashader import ResamplingOperation, rasterize, regrid
 from holoviews.operation import contours
-from holoviews.streams import Stream, FreehandDraw
+from holoviews.streams import Stream, FreehandDraw, Params, BoxEdit
 
 
 class rasterize_polygon(ResamplingOperation):
@@ -71,7 +75,7 @@ class extract_foreground(Operation):
             return element.clone([], vdims=['Foreground'], new_type=gv.Image,
                                  crs=element.crs)
 
-        mask = np.where(fg_mask, fg_mask, 2)
+        mask = np.where(fg_mask, 1, 2)
         mask = np.where(bg_mask, 0, mask).copy()
         bgdModel = np.zeros((1,65), np.float64)
         fgdModel = np.zeros((1,65), np.float64)
@@ -89,10 +93,11 @@ class extract_foreground(Operation):
                              crs=element.crs)
 
 
-
 class filter_polygons(Operation):
 
     minimum_size = param.Integer(default=10)
+
+    link_inputs = param.Boolean(default=True)
 
     def _process(self, element, key=None):
         paths = []
@@ -105,10 +110,9 @@ class filter_polygons(Operation):
         return element.clone(paths)
 
 
-
-class GrabCutDashboard(Stream):
+class GrabCutPanel(param.Parameterized):
     """
-    Defines a Dashboard for extracting contours from an Image.
+    Defines a Panel for extracting contours from an Image.
     """
 
     crs = param.ClassSelector(default=ccrs.PlateCarree(), class_=ccrs.Projection,
@@ -119,11 +123,13 @@ class GrabCutDashboard(Stream):
                                     precedence=-1, is_instance=False, doc="""
         The element type to draw into.""")
 
-    update_contour = param.Action(default=lambda self: self.event(), precedence=1,
-                               doc="""Button triggering GrabCut.""")
+    update_contour = param.Action(default=lambda o: o.param.trigger('update_contour'),
+                                  precedence=1, doc="""
+        Button triggering GrabCut.""")
 
-    filter_contour = param.Action(default=lambda self: self.filter_stream.event(filter=True), precedence=1,
-                                  doc="""Button triggering GrabCut.""")
+    filter_contour = param.Action(default=lambda o: o.param.trigger('filter_contour'),
+                                  precedence=1, doc="""
+        Button triggering GrabCut.""")
 
     width = param.Integer(default=500, precedence=-1, doc="""
         Width of the plot""")
@@ -141,16 +147,19 @@ class GrabCutDashboard(Stream):
 
     def __init__(self, image, fg_data=[], bg_data=[], **params):
         self.image = image
-        super(GrabCutDashboard, self).__init__(transient=True, **params)
+        super(GrabCutPanel, self).__init__(**params)
         self.bg_paths = gv.project(self.path_type(bg_data, crs=self.crs), projection=image.crs)
         self.fg_paths = gv.project(self.path_type(fg_data, crs=self.crs), projection=image.crs)
         self.draw_bg = FreehandDraw(source=self.bg_paths)
         self.draw_fg = FreehandDraw(source=self.fg_paths)
-        self.filter_stream = hv.streams.Stream.define('Filter', filter=False)(transient=True)
+        self._filter_stream = Params(self, ['filter_contour'], transient=True)
+
         self._initialized = False
         self._filter = False
 
+    @param.depends('update_contour')
     def extract_foreground(self, **kwargs):
+        print("EXTRACTING")
         img = self.image
         if self._initialized:
             bg, fg = self.draw_bg.element, self.draw_fg.element
@@ -177,16 +186,169 @@ class GrabCutDashboard(Stream):
         self.result = gv.project(foreground, projection=self.crs)
         return foreground
 
-    def filter_contours(self, obj, **kwargs):
-        if kwargs.get('filter'):
+    def _filter_contours(self, obj, **kwargs):
+        if self._filter_stream._triggering:
             obj = filter_polygons(obj, minimum_size=self.minimum_size)
         self.result = obj
         return obj
 
     def view(self):
         options = dict(width=self.width, height=self.height, xaxis=None, yaxis=None,
-                       projection=self.image.crs)
-        dmap = hv.DynamicMap(self.extract_foreground, streams=[self])
-        dmap = hv.util.Dynamic(dmap, operation=self.filter_contours, streams=[self.filter_stream])
+                       projection=self.image.crs, clone=False)
+        dmap = hv.DynamicMap(self.extract_foreground)
+        dmap = hv.util.Dynamic(dmap, operation=self._filter_contours,
+                               streams=[self._filter_stream])
         return (regrid(self.image).options(**options) * self.bg_paths * self.fg_paths +
                 dmap.options(**options)).options(merge_tools=False, clone=False)
+
+
+
+class SelectRegionPanel(param.Parameterized):
+    """
+    Visualization that allows selecting a bounding box anywhere on a tile source.
+    """
+
+    # Tile servers
+    misc_servers =  {'OpenStreetMap' : 'http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png',
+                     'Basemaps CartoCDN' :
+                     'https://s.basemaps.cartocdn.com/light_all/{Z}/{X}/{Y}.png',
+                     'Stamen':'http://tile.stamen.com/terrain/{Z}/{X}/{Y}.png'}
+
+    arcgis_paths = {'World Imagery':'World_Imagery/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Topo Map':'World_Topo_Map/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Terrain Base':'World_Terrain_Base/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Street Map': 'World_Street_Map/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Shaded Relief': 'World_Shaded_Relief/MapServer/tile/{Z}/{Y}/{X}',
+                    'World Physical Map': 'World_Physical_Map/MapServer/tile/{Z}/{Y}/{X}',
+                    'USA Topo Maps' : 'USA_Topo_Maps/MapServer/tile/{Z}/{Y}/{X}',
+                    'Ocean Basemap' : 'Ocean_Basemap/MapServer/tile/{Z}/{Y}/{X}',
+                    'NatGeo World Map' : 'NatGeo_World_Map/MapServer/tile/{Z}/{Y}/{X}'}
+
+    arcgis_urls = {k: 'https://server.arcgisonline.com/ArcGIS/rest/services/' + v
+                   for k,v in arcgis_paths.items()}
+
+    tile_urls = dict(misc_servers, **arcgis_urls)
+
+    # Parameters
+    name = param.String(default='Region Settings')
+
+    width = param.Integer(default=900, precedence=-1, doc="Width of the plot in pixels")
+
+    height = param.Integer(default=700, precedence=-1, doc="Height of the plot in pixels")
+
+    zoom_level = param.Integer(default=7, bounds=(1,21), precedence=-1, doc="""
+       The zoom level is updated when the bounding box is drawn."""   )
+
+    tile_server = param.ObjectSelector(default=tile_urls['World Imagery'], objects=tile_urls)
+
+    magnification = param.Integer(default=1, bounds=(1,10), precedence=0.1)
+
+    def __init__(self, poly_data=[], **params):
+        super(SelectRegionPanel, self).__init__(**params)
+        self.boxes = gv.Polygons(poly_data).options(
+            fill_alpha=0.5, color='grey', line_color='white',
+            line_width=2, width=self.width, height=self.height
+        )
+        self.box_stream = BoxEdit(source=self.boxes, num_objects=1)
+
+    @classmethod
+    def bounds_to_zoom_level(cls, bounds, width, height,
+                             tile_width=256, tile_height=256, max_zoom=21):
+        """
+        Computes the zoom level from the lat/lon bounds and the plot width and height
+        bounds: tuple(float)
+            Bounds in the form (lon_min, lat_min, lon_max, lat_max)
+        width: int
+            Width of the overall plot
+        height: int
+            Height of the overall plot
+        tile_width: int (default=256)
+            Width of each tile
+        tile_width: int (default=256)
+            Height of each tile
+        max_zoom: int (default=21)
+            Maximum allowed zoom level
+        """
+
+        def latRad(lat):
+            sin = math.sin(lat * math.pi / 180);
+            if sin == 1:
+                radX2 = 20
+            else:
+                radX2 = math.log((1 + sin) / (1 - sin)) / 2;
+            return max(min(radX2, math.pi), -math.pi) / 2;
+
+        def zoom(mapPx, worldPx, fraction):
+            return math.floor(math.log(mapPx / worldPx / fraction) / math.log(2));
+
+        x0, y0, x1, y1 = bounds
+        latFraction = (latRad(y1) - latRad(y0)) / math.pi
+        lngDiff = x1 - x0
+        lngFraction = ((lngDiff + 360) if lngDiff < 0 else lngDiff)/360
+        latZoom = zoom(height, tile_height, latFraction)
+        lngZoom = zoom(width, tile_width, lngFraction)
+        return min(latZoom, lngZoom, max_zoom)
+
+    @param.depends('tile_server')
+    def callback(self):
+        return (gv.WMTS(self.tile_server) * gv.tile_sources.StamenLabels())
+
+    @property
+    def bbox(self):
+        element = self.box_stream.element
+        # Update shared_state with bounding box (if any)
+        if element:
+            xs, ys = element.array().T
+            bbox = (xs[0], ys[0], xs[2], ys[1])
+            # Set the zoom level
+            zoom_level = self.bounds_to_zoom_level(bbox, self.width, self.height)
+            self.zoom_level = zoom_level + self.magnification
+            return bbox
+        else:
+            return None
+
+    def view(self):
+        return (gv.DynamicMap(self.callback) * self.boxes)
+
+    def get_tiff(self):
+        bbox = self.bbox
+        filepath = self.tiff_from_bbox(self.tile_server, self.zoom_level, bbox)
+        return gv.load_tiff(filepath, crs=ccrs.PlateCarree()).redim(x='Longitude', y='Latitude')
+
+    @classmethod
+    def tiff_from_bbox(cls, tile_server, zoom_level, bbox):
+        if bbox is None:
+            raise ValueError('Please supply a bounding box in order to extract a tiff.')
+
+        options = {'url': tile_server, 'zoom_level': zoom_level,
+                   'bbox': bbox, 'crop_to_bbox':True}
+
+        quest_service = 'svc://wmts:seamless_imagery'
+        tile_service_options = quest.api.download_options(quest_service,
+                                                          fmt='param')[quest_service]
+        basemap_features = quest.api.get_features(quest_service)
+        collection_name = 'examples'
+        if collection_name in quest.api.get_collections():
+            pass
+        else:
+            quest.api.new_collection(collection_name)
+
+        collection_feature = quest.api.add_features(collection_name, basemap_features[0])
+
+        staged_id = quest.api.stage_for_download(collection_feature, options=options)
+        download_state = quest.api.download_datasets(staged_id)
+        # Fragile, needs improvement
+        download_id = list(download_state.keys())[0]
+
+        meta = quest.api.get_metadata(staged_id)
+        file_path = meta[download_id].get('file_path',None)
+        if not os.path.isfile(file_path):
+            print('Error: No TIFF downloaded')
+        return file_path
+
+
+options = Store.options('bokeh')
+
+options.Points = Options('plot', padding=0.1)
+options.Path = Options('plot', padding=0.1)
+options.Polygons = Options('plot', padding=0.1)
